4f23b0506dd3d8f1daf86d84d59d9896a7cbe057
rogata <agata85rojek@gmail.com>
2018-05-22 00:30:29 +0200

    add section

--- wp-content/themes/medpuls_theme/gallery.php
+++ wp-content/themes/medpuls_theme/gallery.php
@@ -4,5 +4,13 @@ Template Name: Gallery Template
 Template Post Type: page
 */
 get_header(); ?>
-<?php photo_gallery(1); ?>
+<section class="section section-icons #80cbc4 teal lighten-3 center">
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m10">
+                <?php photo_gallery(1); ?>
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
